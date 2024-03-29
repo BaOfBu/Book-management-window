9e7526969544926ff0ef0c713c3d08939858757b
BaOfBu <111734920+BaOfBu@users.noreply.github.com>
2024-03-29 16:57:42 +0700

    temp commit for home page

--- MyShop/Flora/DbContexts/MyShopContext.cs
+++ MyShop/Flora/DbContexts/MyShopContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using Microsoft.EntityFrameworkCore;
 
 namespace Flora;
@@ -30,8 +31,11 @@ public partial class MyShopContext : DbContext
     public virtual DbSet<UserAccount> UserAccounts { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=.; Trusted_Connection=Yes; Initial Catalog=MyShop; TrustServerCertificate=True");
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- MyShop/Flora/ViewModel/HomeVM.cs
+++ MyShop/Flora/ViewModel/HomeVM.cs
@@ -1,12 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using Flora.Model;
 using System.Threading.Tasks;
+using Flora.View;
+using Microsoft.EntityFrameworkCore;
 
 namespace Flora.ViewModel
 {
-    class HomeVM
+    class HomeVM : Utilities.ViewModelBase
     {
+        private MyShopContext _shopContext;
+        public ObservableCollection<HomeProductPreview> Plants { get; set; }
+        public HomeVM()
+        {
+            _shopContext = new MyShopContext();
+            Plants = get5LeastProduct();
+            
+        }   
+
+        //private List<Plant> get5LeastProduct()
+        //{
+        //    var query = from p in _shopContext.Plants
+        //                orderby p.StockQuantity
+        //                select p;
+
+        //    return query.Take(5).ToList();
+        //}
+
+        private ObservableCollection<HomeProductPreview> get5LeastProduct()
+        {
+            var query = _shopContext.Plants
+                        .OrderBy(p => p.StockQuantity)
+                        .Include(p => p.Category);
+            var products = query.Take(5).ToList();
+            int productIndex = 1;
+
+            var plants = new ObservableCollection<HomeProductPreview>();
+            foreach (var o in products)
+            {
+                plants.Add(new HomeProductPreview
+                {
+                    ProductIndex = productIndex++,
+                    PlantId = o.PlantId,
+                    Name = o.Name,
+                    Quantity = o.StockQuantity,
+                    Price = (decimal)o.Price,
+                    CategoryName = o.Category.CategoryName
+                });
+            }
+
+            return plants;
+        }
     }
 }
