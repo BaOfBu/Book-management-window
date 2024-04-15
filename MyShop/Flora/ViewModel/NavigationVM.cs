ea0b26a2771a114ce469644aecc88da0d4cb541f
BaOfBu <111734920+BaOfBu@users.noreply.github.com>
2024-04-15 15:07:20 +0700

    Add restore last visited page

--- MyShop/Flora/ViewModel/NavigationVM.cs
+++ MyShop/Flora/ViewModel/NavigationVM.cs
@@ -1,6 +1,7 @@
 ﻿using Flora.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Windows.Input;
 
 namespace Flora.ViewModel
@@ -19,6 +20,97 @@ namespace Flora.ViewModel
             {
                 _currentView = value;
                 OnPropertyChanged();
+            }
+        }
+        private bool _homeIsChecked;
+        public bool HomeIsChecked
+        {
+            get { return _homeIsChecked; }
+            set
+            {
+                if (_homeIsChecked != value)
+                {
+                    _homeIsChecked = value;
+                    OnPropertyChanged(nameof(HomeIsChecked));
+                }
+            }
+        }
+        private bool _categoryIsChecked;
+        public bool CategoryIsChecked
+        {
+            get { return _categoryIsChecked; }
+            set
+            {
+                if (_categoryIsChecked != value)
+                {
+                    _categoryIsChecked = value;
+                    OnPropertyChanged(nameof(CategoryIsChecked));
+                }
+            }
+        }
+        private bool _productIsChecked;
+        public bool ProductIsChecked
+        {
+            get { return _productIsChecked; }
+            set
+            {
+                if (_productIsChecked != value)
+                {
+                    _productIsChecked = value;
+                    OnPropertyChanged(nameof(ProductIsChecked));
+                }
+            }
+        }
+        private bool _orderIsChecked;
+        public bool OrderIsChecked
+        {
+            get { return _orderIsChecked; }
+            set
+            {
+                if (_orderIsChecked != value)
+                {
+                    _orderIsChecked = value;
+                    OnPropertyChanged(nameof(OrderIsChecked));
+                }
+            }
+        }
+        private bool _voucherIsChecked;
+        public bool VoucherIsChecked
+        {
+            get { return _voucherIsChecked; }
+            set
+            {
+                if (_voucherIsChecked != value)
+                {
+                    _voucherIsChecked = value;
+                    OnPropertyChanged(nameof(VoucherIsChecked));
+                }
+            }
+        }
+        private bool _reportIsChecked;
+        public bool ReportIsChecked
+        {
+            get { return _reportIsChecked; }
+            set
+            {
+                if (_reportIsChecked != value)
+                {
+                    _reportIsChecked = value;
+                    OnPropertyChanged(nameof(ReportIsChecked));
+                }
+            }
+        }
+        private bool _backupIsChecked;
+        public bool BackupIsChecked
+        {
+            get { return _backupIsChecked; }
+            set
+            {
+                if (_backupIsChecked != value)
+                {
+                    _backupIsChecked = value;
+                    OnPropertyChanged(nameof(BackupIsChecked));
+                }
             }
         }
         public ICommand HomeCommand { get; set; }
@@ -97,17 +189,65 @@ namespace Flora.ViewModel
                 NavigateToWithParameter(typeof(EditPlantProductVM), plant);
             });
             BackupCommand = new RelayCommand(Backup);
-            CurrentView = new HomeVM();
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var lastWindow = config.AppSettings.Settings["lastWindow"].Value;
+            if (!string.IsNullOrEmpty(lastWindow))
+            {
+                var viewModelType = Type.GetType($"Flora.ViewModel.{lastWindow}");
+                if (viewModelType != null)
+                {
+                    var viewModelInstance = Activator.CreateInstance(viewModelType);
+                    if (viewModelInstance != null)
+                    {
+                        CurrentView = viewModelInstance;
+                    }
+                    switch (viewModelType)
+                    {
+                        case Type t when t == typeof(HomeVM):
+                            HomeIsChecked = true;
+                            break;
+                        case Type t when t == typeof(ProductVM):
+                            CategoryIsChecked = true;
+                            break;
+                        case Type t when t == typeof(PlantVM):
+                            ProductIsChecked = true;
+                            break;
+                        case Type t when t == typeof(OrderVM):
+                            OrderIsChecked = true;
+                            break;
+                        case Type t when t == typeof(VoucherVM):
+                            VoucherIsChecked = true;
+                            break;
+                        case Type t when t == typeof(ReportVM):
+                            ReportIsChecked = true;
+                            break;
+                        case Type t when t == typeof(BackupVM):
+                            BackupIsChecked = true;
+                            break;
+                        
+
+                    }
+                }
+            }
+            else
+            {
+                CurrentView = new HomeVM();
+            }
+
 
         }
         public void ChangeViewMethod(Type viewModelType)
         {
             BeforeViewChange?.Invoke(this, EventArgs.Empty);
             var viewModelInstance = Activator.CreateInstance(viewModelType);
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
             if (viewModelInstance != null)
             {
                 navigationHistory.Push(CurrentView);
                 CurrentView = viewModelInstance;
+                config.AppSettings.Settings["lastWindow"].Value = CurrentView.GetType().Name;
+                config.Save(ConfigurationSaveMode.Minimal);
+                ConfigurationManager.RefreshSection("appSettings");
             }
 
         }
